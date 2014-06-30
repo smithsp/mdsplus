a260c4d08712e28216c2adf4c49688c34717a740
Thomas W. Fredian <twf@psfc.mit.edu>
2014-06-30 13:21:13 +0000

    Cleanup removing VXWORKS and using _MSC_VER to detect Visual C compiler

--- mdsshr/MdsShrThreadSafe.c
+++ mdsshr/MdsShrThreadSafe.c
@@ -12,26 +12,22 @@ STATIC_THREADSAFE pthread_once_t buffer_key_once = PTHREAD_ONCE_INIT;
 
 STATIC_ROUTINE void buffer_key_alloc();
 
-#if defined HAVE_WINDOWS_H && !defined HAVE_PTHREAD_H
-
-void pthread_detach(HANDLE *thread)
-{
+#if defined _MSC_VER
+############### Emulate pthreads for Visual C applications #######################
+void pthread_detach(HANDLE *thread) {
 	return;
 }
 
-int pthread_cond_init(HANDLE *cond, void *def)
-{
+int pthread_cond_init(HANDLE *cond, void *def) {
   *cond = CreateEvent(NULL,TRUE,FALSE,NULL);
   return (*cond == NULL);
 }
 
-BOOL pthread_cond_destroy(HANDLE *cond)
-{
+BOOL pthread_cond_destroy(HANDLE *cond) {
    return CloseHandle(*cond);
 }
 
-int pthread_cond_signal(HANDLE *cond)
-{
+int pthread_cond_signal(HANDLE *cond) {
   BOOL status;
 #ifdef ___DEBUG_IT
   printf("signalling event %p\n",*cond);
@@ -48,8 +44,7 @@ int pthread_cond_signal(HANDLE *cond)
   return status == 0;
 }
 
-int pthread_cond_wait(HANDLE *cond, HANDLE *mutex)
-{
+int pthread_cond_wait(HANDLE *cond, HANDLE *mutex) {
 	int status;
 #ifdef ___DEBUG_IT
    printf("waiting for condition %p\n",*cond);
@@ -64,8 +59,7 @@ int pthread_cond_wait(HANDLE *cond, HANDLE *mutex)
 }
 
 #define ETIMEDOUT 138
-int pthread_cond_timedwait(HANDLE *cond, HANDLE *mutex, int msec)
-{
+int pthread_cond_timedwait(HANDLE *cond, HANDLE *mutex, int msec) {
    int status;
    pthread_mutex_unlock(mutex);
    status = WaitForSingleObject(*cond,msec);
@@ -77,24 +71,20 @@ int pthread_cond_timedwait(HANDLE *cond, HANDLE *mutex, int msec)
    return status;
 }
 
-int pthread_mutex_init(HANDLE *mutex, void *dummy)
-{
+int pthread_mutex_init(HANDLE *mutex, void *dummy) {
   *mutex = CreateMutex(0,FALSE,NULL);
   return (*mutex == NULL);
 }
 
-BOOL pthread_mutex_destroy(HANDLE *mutex)
-{
+BOOL pthread_mutex_destroy(HANDLE *mutex) {
   return CloseHandle(*mutex);
 }
 
-int pthread_exit(int status)
-{
+int pthread_exit(int status) {
 	return status;
 }
 
-int pthread_create(pthread_t *thread, void *dummy, void *(*rtn)(void *), void *rtn_param)
-{
+int pthread_create(pthread_t *thread, void *dummy, void *(*rtn)(void *), void *rtn_param) {
   *thread = (pthread_t)_beginthread( (void (*)(void *))rtn, 0, rtn_param);
   return *thread == 0;
 }
@@ -103,8 +93,7 @@ void pthread_cleanup_pop(){}
 
 void pthread_cleanup_push(){}
 
-int pthread_mutex_lock(HANDLE *mutex)
-{
+int pthread_mutex_lock(HANDLE *mutex) {
 	int status;
 #ifdef ___DEBUG_IT
    printf("Trying to lock mutex %p\n",*mutex);
@@ -116,8 +105,7 @@ int pthread_mutex_lock(HANDLE *mutex)
    return status;
 }
 
-int pthread_mutex_unlock(HANDLE *mutex)
-{
+int pthread_mutex_unlock(HANDLE *mutex) {
 	int status;
 #ifdef ___DEBUG_IT
 	printf("Unlocking mutex %p\n",*mutex);
@@ -129,13 +117,11 @@ int pthread_mutex_unlock(HANDLE *mutex)
 	return status;
 }
 
-void pthread_cancel(HANDLE thread)
-{
+void pthread_cancel(HANDLE thread) {
 	printf("Abort not supported");
 }
 
-void pthread_once(pthread_once_t *one_time,void (*key_alloc)())
-{
+void pthread_once(pthread_once_t *one_time,void (*key_alloc)()) {
   if (*one_time == PTHREAD_ONCE_INIT)
   {
     *one_time = 1;
@@ -143,76 +129,17 @@ void pthread_once(pthread_once_t *one_time,void (*key_alloc)())
   }
 }
 
-void *pthread_getspecific(pthread_key_t buffer_key)
-{
+void *pthread_getspecific(pthread_key_t buffer_key) {
   return TlsGetValue(buffer_key);
 }
 
-void pthread_setspecific(pthread_key_t buffer_key, void *p)
-{
+void pthread_setspecific(pthread_key_t buffer_key, void *p) {
   TlsSetValue(buffer_key,p);
 }
 
-void pthread_key_create(pthread_key_t *buffer_key,void *d2){*buffer_key=TlsAlloc();}
-#endif
-#ifdef HAVE_VXWORKS_H
-#define HANDLE void*
-void pthread_once(pthread_once_t *one_time,void (*key_alloc)())
-{
-  if (*one_time == PTHREAD_ONCE_INIT)
-  {
-    *one_time = 1;
-    key_alloc();
-  }
+void pthread_key_create(pthread_key_t *buffer_key,void *d2){
+  *buffer_key=TlsAlloc();
 }
-
-static int bufferKeys[1024];
-static void *pthreadKeys[1024];
-static int numKeys;
-
-static void TlsSetValue(int bufferKey, void *p)
-{
-	int i;
-	for(i = 0; i < numKeys; i++)
-	{
-		if(bufferKeys[i] == bufferKey)
-			break;
-	}
-	if(i == numKeys)
-		numKeys++;
-	bufferKeys[i] = bufferKey;
-	pthreadKeys[i] = p;
-}
-
-static void *TlsGetValue(int bufferKey)
-{
-	int i;
-	for(i = 0; i < numKeys; i++)
-	{
-		if(bufferKeys[i] == bufferKey)
-			break;
-	}
-	return pthreadKeys[i];
-}
-
-
-
-
-void *pthread_getspecific(pthread_key_t buffer_key)
-{
-  return TlsGetValue(buffer_key);
-}
-
-void pthread_setspecific(pthread_key_t buffer_key, void *p)
-{
-  TlsSetValue(buffer_key,p);
-}
-
-//void pthread_key_create(pthread_key_t *buffer_key,void *d2){*buffer_key=TlsAlloc();}
-void pthread_key_create(pthread_key_t *buffer_key,void *d2){*buffer_key=malloc(sizeof(int *));}
-extern void pthread_mutex_init(HANDLE *mutex);
-extern void pthread_mutex_lock(HANDLE *mutex);
-extern void pthread_mutex_unlock(HANDLE *mutex);
 #endif
 
 /* Return the thread-specific buffer */
@@ -248,24 +175,19 @@ STATIC_ROUTINE void buffer_key_alloc()
    pthread_key_create(&buffer_key, buffer_destroy);
 }
 
-#ifndef HAVE_WINDOWS_H
-#ifndef HAVE_VXWORKS_H
+#ifndef _MSC_VER
 pthread_mutex_t  initMutex = PTHREAD_MUTEX_INITIALIZER;
-#endif
 #endif
 
 
 void LockMdsShrMutex(pthread_mutex_t *mutex,int *initialized)
 {
-#ifndef HAVE_WINDOWS_H
-#ifndef HAVE_VXWORKS_H
+#ifndef _MSC_VER
   pthread_mutex_lock(&initMutex);
-#endif
 #endif
   if(!*initialized)
   {
-#ifndef HAVE_WINDOWS_H
-#ifndef HAVE_VXWORKS_H
+#ifndef _MSC_VER
     pthread_mutexattr_t m_attr;
     pthread_mutexattr_init(&m_attr);
 #if !defined(PTHREAD_MUTEX_RECURSIVE)
@@ -281,10 +203,8 @@ void LockMdsShrMutex(pthread_mutex_t *mutex,int *initialized)
 #endif
     *initialized = 1;
   }
-#ifndef HAVE_WINDOWS_H
-#ifndef HAVE_VXWORKS_H
+#ifndef _MSC_VER
   pthread_mutex_unlock(&initMutex);
-#endif
 #endif
   pthread_mutex_lock(mutex);
 
@@ -295,7 +215,7 @@ void UnlockMdsShrMutex(pthread_mutex_t *mutex)
   pthread_mutex_unlock(mutex);
 }
 
-#ifndef HAVE_PTHREAD_H
+#ifdef _MSC_VER
 STATIC_THREADSAFE HANDLE global_mutex = NULL;
 #else
 STATIC_THREADSAFE pthread_mutex_t global_mutex;
